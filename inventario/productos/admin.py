ce193559868261e18e95afe9692a501026818ccb
Your Name <you@example.com>
2025-11-14 19:57:21 -0300

    primer commit

---
+++ inventario/productos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Producto
+
+# Register your models here.
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'precio', 'stock', 'necesita_reposicion']
+    list_filter = ['stock']
+    search_fields = ['nombre']
